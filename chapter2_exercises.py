38c10143234514fac5a1b8c7a67e151400e90c3e
Brian Yee <yee.brianj@gmail.com>
2013-03-29 02:04:47 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,23 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: Brian Yee
+
+# 2.1
+# Python thinks this variable with a leading zero is an octal instead of an integer
+
+# 2.2
+print 5
+print x = 5
+print x + 1
+
+# 2.3
+# 1. 8 int
+# 2. 8.5 float
+# 3. 4.0 float
+# 4. 11 int
+# 5. '.....' str
+
+# 2.4
+# 1. 523.6
+# 2. $62.22
+# 3. 7:30:06 am
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,45 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: Brian Yee
+
+# 3.1
+# NameError: name 'repeat_lyrics' is not defined
+
+# 3.2
+# Works fine. Prints the following:
+# I'm a lumberjack, and I'm okay.
+# I sleep all night and I work all day.
+# I'm a lumberjack, and I'm okay.
+# I sleep all night and I work all day.
+
+# 3.3
+def right_justify(s):
+    print ' ' * (70 - len(s)) + s
+
+# 3.4
+def do_twice(f, value):
+    f(value)
+    f(value)
+
+def print_spam():
+    print 'spam'
+
+def print_twice(s):
+    print s
+    print s
+
+def do_four(f, value):
+    do_twice(f, value)
+    do_twice(f, value)
+
+# prints 'spam' once
+#print_spam()
+
+# prints 'spam' twice
+#print_twice('spam')
+
+# prints 'spam' four times
+#do_twice(print_twice, 'spam')
+
+# prints 'spam' eight times
+#do_four(print_twice,'spam')
